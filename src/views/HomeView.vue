8482bb52ab49eba42adc46e89555f36ae638ef4d
krekas <krekas@gmail.com>
2025-07-16 14:47:41 +0300

    fetch and show data from a Laravel API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue';
+
+const categories = ref({})
+const products = ref({})
+
+const getCategories = async () => {
+	await fetch('http://demo.test/api/categories')
+		.then(response => response.json())
+		.then(data => {
+			categories.value = data.data
+		})
+		.catch((error) => console.log(error))
+}
+
+const getProducts = async () => {
+	await fetch('http://demo.test/api/products')
+		.then(response => response.json())
+		.then(data => {
+			products.value = data.data
+		})
+		.catch((error) => console.log(error))
+}
+
+onMounted(() => {
+	getCategories()
+	getProducts()
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<div>
+		<ul style="display: inline-block;margin-bottom: 10px;">
+			<li style="display: inline-block;" v-for="category in categories" :key="category.id">
+				<a href="#">{{ category.name }}</a>
+			</li>
+		</ul>
+		
+		<ul>
+			<li v-for="product in products" :key="product.id">
+				<strong>{{ product.name }}</strong> ${{ product.price }} <em>({{ product.category.name }})</em>
+			</li>
+		</ul>
+	</div>
 </template>

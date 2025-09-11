0f6dab93467260ed04d2d88f61fdbbb6b7e350b4
krekas <krekas@gmail.com>
2025-09-11 14:41:07 +0300

    Upload file

--- src/views/categories/CreateView.vue
+++ src/views/categories/CreateView.vue
@@ -5,14 +5,19 @@ const form = reactive({})
 const errors = ref({})
 
 const save = async () => {
+	const formData = new FormData()
+
+    Object.keys(form).forEach(key => {
+        if (form[key] != null) formData.append(key, form[key])
+    })
+	
 	const response = await fetch('http://api.test/api/categories/', {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json',
 			'Accept': 'application/json',
 			'X-Requested-With': 'XMLHttpRequest',
 		},
-		body: JSON.stringify(form)
+		body: formData
 	})
 	
 	const data = await response.json()
@@ -22,6 +27,14 @@ const save = async () => {
 		// Redirect to another page.
 	} else {
 		errors.value = data.errors;
+	}
+}
+
+const handleFileChange = (event) => {
+	const file = event.target.files[0]
+	
+	if (file) {
+		form.photo = file
 	}
 }
 </script>
@@ -38,6 +51,12 @@ const save = async () => {
 			<div>
 				<label class="block" for="description">Description</label>
 				<textarea v-model="form.description" type="text" name="description" id="description" class="border border-gray-300 rounded p-2"></textarea>
+			</div>
+			
+			<div>
+				<label class="block" for="photo">Photo</label>
+				<input @change="handleFileChange" type="file" name="photo" id="photo" class="border border-gray-300 rounded p-2" />
+				<div v-if="errors.photo" class="text-xs text-rose-400">{{ errors.photo[0] }}</div>
 			</div>
 	
 			<div>
